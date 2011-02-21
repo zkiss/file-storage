015a4ad37b1f862b3751a4a4ee2cd46247e348a0
zoltan.kiss.jr@gmail.com <zoltan.kiss.jr@gmail.com@2ae42001-0750-2142-ea0c-33c2199f0e23>
2011-02-21 15:06:04 +0000

    converter

---
+++ trunk/fs-web/src/hu/bme/vihijv37/bus1fj/web/server/converter/ConverterConfig.java
@@ -0,0 +1,78 @@
+package hu.bme.vihijv37.bus1fj.web.server.converter;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.TreeMap;
+
+import hu.bme.vihijv37.bus1fj.web.server.converter.conversion.ConversionDefinition;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+class ConverterConfig {
+
+    private static final Log log = LogFactory.getLog(ConverterConfig.class);
+
+    /**
+     * Beégetett értékek
+     */
+    private static final LinkedList<ConversionDefinition> CONVERSION_DEFINITIONS;
+
+    /**
+     * Beégetett konfig példány
+     */
+    private static final ConverterConfig BURNT_IN_CONFIG;
+
+    static {
+	CONVERSION_DEFINITIONS = new LinkedList<ConversionDefinition>();
+
+	// TODO config
+	// ConverterConfig.CONVERSION_DEFINITIONS.add(new
+	// ConversionDefinition(User.class, User.class));
+
+	BURNT_IN_CONFIG = new ConverterConfig(ConverterConfig.CONVERSION_DEFINITIONS);
+    }
+
+    public static ConverterConfig getBurntInConfig() {
+	return ConverterConfig.BURNT_IN_CONFIG;
+    }
+
+    /**
+     * Kulcs: osztálynév<br>
+     * Érték: {@link Conversion}
+     */
+    private final TreeMap<String, Conversion> conversions;
+
+    private ConverterConfig(Collection<ConversionDefinition> conversionDefinitions) {
+	this.conversions = new TreeMap<String, Conversion>();
+
+	if (ConverterConfig.log.isInfoEnabled()) {
+	    StringBuilder sb = new StringBuilder("Converter config:");
+	    for (ConversionDefinition cp : conversionDefinitions) {
+		sb.append("\n").append(cp);
+	    }
+	    ConverterConfig.log.info(sb);
+	}
+
+	this.buildConversions(conversionDefinitions);
+    }
+
+    private void buildConversions(Collection<ConversionDefinition> conversionDefinitions) {
+	for (ConversionDefinition cd : conversionDefinitions) {
+	    Conversion conversion = new Conversion(cd);
+	    this.conversions.put(cd.class1.getName(), conversion);
+	    this.conversions.put(cd.class2.getName(), conversion);
+	}
+    }
+
+    /**
+     * A JavaBean-hoz tartozó {@link Conversion}
+     * 
+     * @param clazz
+     * @return
+     */
+    public Conversion getConversion(Class<?> clazz) {
+	return this.conversions.get(clazz.getName());
+    }
+
+}
---
+++ trunk/fs-web/src/hu/bme/vihijv37/bus1fj/web/server/converter/conversion/Skip.java
@@ -0,0 +1,14 @@
+package hu.bme.vihijv37.bus1fj.web.server.converter.conversion;
+
+/**
+ * A property ne legyen átmentve
+ * 
+ * @author Zoltan Kiss
+ */
+public class Skip extends ConversionRule {
+
+    public Skip(String property) {
+	super(property);
+    }
+
+}
