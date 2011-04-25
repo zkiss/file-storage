98c63b67197781f480f8635601de9a980e3026e9
zoltan.kiss.jr@gmail.com <zoltan.kiss.jr@gmail.com@2ae42001-0750-2142-ea0c-33c2199f0e23>
2011-04-25 22:18:31 +0000

    service, dao

--- fs-web/src/hu/bme/vihijv37/bus1fj/web/client/FsWeb.java
+++ fs-web/src/hu/bme/vihijv37/bus1fj/web/client/FsWeb.java
@@ -1,14 +1,11 @@
 package hu.bme.vihijv37.bus1fj.web.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import hu.bme.vihijv37.bus1fj.web.client.components.LoginScreen;
 
 public class FsWeb implements EntryPoint {
-
-    private final FsServiceAsync greetingService = GWT.create(FsService.class);
 
     @Override
     public void onModuleLoad() {
